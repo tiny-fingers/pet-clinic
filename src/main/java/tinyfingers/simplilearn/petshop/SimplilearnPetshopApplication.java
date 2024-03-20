53c4fe89230ac187e47e3ea5a89000845fa41dba
tinyfingers <tiny.fingers@outlook.com>
2024-03-20 22:04:13 +0100

    Initial commit

---
+++ src/main/java/tinyfingers/simplilearn/petshop/SimplilearnPetshopApplication.java
@@ -0,0 +1,13 @@
+package tinyfingers.simplilearn.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnPetshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplilearnPetshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tinyfingers/simplilearn/petshop/SimplilearnPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package tinyfingers.simplilearn.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnPetshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
